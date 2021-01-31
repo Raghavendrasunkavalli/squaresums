82d796a4b371bf85628ab881c1d1111fdb4821cb
Venkata Sai Raghavendra Rao Sunkavalli <sunkavalli2@usf.edu>
2021-01-30 22:21:22 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace squaresums
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+           if (squaresum())
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+        static bool squaresum()
+        {
+            Console.Write("Enter the number:");
+            string c1 = Console.ReadLine();
+            int c = Convert.ToInt32(c1);
+            for (int a = 0; a * a <= c; a++)
+            {
+                for (int b = 0; b * b <= c; b++)
+                {
+                    if (a * a + b * b == c)
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
